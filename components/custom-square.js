96fbc344ba1e7d8c35202c5d1e863c1ec8cb3b34
Reichel J. Yanez <reichelrei@gmail.com>
2018-09-05 17:52:09 -0400

    push

---
+++ components/custom-square.js
@@ -0,0 +1,40 @@
+// Create a class for the element
+class Square extends HTMLElement {
+  // Specify observed attributes so that
+  // attributeChangedCallback will work
+  static get observedAttributes() {
+    return ['w', 'l'];
+  }
+
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    const shadow = this.attachShadow({mode: 'open'});
+
+    const div = document.createElement('div');
+    const style = document.createElement('style');
+    shadow.appendChild(style);
+    shadow.appendChild(div);
+  }
+
+  connectedCallback() {
+    console.log('Custom square element added to page.');
+    updateStyle(this);
+  }
+
+  disconnectedCallback() {
+    console.log('Custom square element removed from page.');
+  }
+
+  adoptedCallback() {
+    console.log('Custom square element moved to new page.');
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('Custom square element attributes changed.');
+    updateStyle(this);
+  }
+}
+
+customElements.define('custom-square', Square);
